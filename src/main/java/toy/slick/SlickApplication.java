f0bf6efa97ae5bf811fbf34a321f8b1cfe1e7d5d
wjdwo951219 <wjdwo951219@gmail.com>
2024-07-11 10:28:51 +0900

    delete gradle impl for feign

--- src/main/java/toy/slick/SlickApplication.java
+++ src/main/java/toy/slick/SlickApplication.java
@@ -3,13 +3,11 @@ package toy.slick;
 import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import toy.slick.common.Const;
 
 import java.util.TimeZone;
 
-@EnableFeignClients
 @EnableScheduling
 @SpringBootApplication
 public class SlickApplication {
